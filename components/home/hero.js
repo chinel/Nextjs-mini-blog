a15e95221e5af9b5a31fb148f970c803af48a624
nelo <nelokwaz@gmail.com>
2021-12-29 13:53:40 +0100

    added the base structure of the hero component

---
+++ components/home/hero.js
@@ -0,0 +1,14 @@
+function Hero() {
+  return (
+    <section>
+      <div>Image</div>
+      <h1>Hi, I'm Nelly</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like React
+        or Next
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
