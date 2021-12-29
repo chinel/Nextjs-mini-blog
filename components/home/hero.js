71fa40ef8cabfd0044cd2033fb3ecfd9b99e33e4
nelo <nelokwaz@gmail.com>
2021-12-29 14:29:09 +0100

    completed the hero component

--- components/home/hero.js
+++ components/home/hero.js
@@ -1,7 +1,16 @@
+import Image from "next/image";
+import classes from "./hero.module.css";
 function Hero() {
   return (
-    <section>
-      <div>Image</div>
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/profile.jpg"
+          alt="An Image showing Nelly"
+          width={300}
+          height={300}
+        />
+      </div>
       <h1>Hi, I'm Nelly</h1>
       <p>
         I blog about web development - especially frontend frameworks like React
