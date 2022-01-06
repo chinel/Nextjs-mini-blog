81fda7655ef61e93aab9f489e992e9f8ed48723f
nelo <nelokwaz@gmail.com>
2022-01-06 22:22:52 +0100

    displayed the post data in the post item component

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -2,17 +2,26 @@ import Image from "next/image";
 import Link from "next/link";
 import classes from "./post-item.module.css";
 function PostItem(props) {
+  const { title, image, excerpt, date, slug } = props.post;
+  const formattedDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const imagePath = `/images/posts/${slug}/${image}`;
+
   return (
     <li className={classes.post}>
       <Link>
         <a>
           <div className={classes.image}>
-            <Image />
+            <Image src={imagePath} alt={title} width={300} height={200} />
           </div>
           <div className={classes.content}>
-            <h3>TITLE</h3>
-            <time>July 13th 2022</time>
-            <p>The excerpt</p>
+            <h3>{title}</h3>
+            <time>{formattedDate}</time>
+            <p>{excerpt}</p>
           </div>
         </a>
       </Link>
