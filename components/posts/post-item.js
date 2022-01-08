45249b5467caeda778de1931bffae62566646256
nelo <nelokwaz@gmail.com>
2022-01-08 23:19:07 +0100

    fxied post item component bug

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -10,10 +10,11 @@ function PostItem(props) {
   });
 
   const imagePath = `/images/posts/${slug}/${image}`;
+  const linkPath = `/posts/${slug}`;
 
   return (
     <li className={classes.post}>
-      <Link>
+      <Link href={linkPath}>
         <a>
           <div className={classes.image}>
             <Image src={imagePath} alt={title} width={300} height={200} />
