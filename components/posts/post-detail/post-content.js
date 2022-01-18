0173325b89dc911cba994d48b76840e939d7ef43
nelo <nelokwaz@gmail.com>
2022-01-18 06:32:17 +0100

    added dummy content to the post content component

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,9 +1,19 @@
 import PostHeader from "./post-header";
+
+const DUMMY_POST = {
+  title: "Getting Started with NextJs",
+  slug: "getting-started-with-nextjs",
+  image: "getting-started-nextjs.png",
+  date: "2022-02-10",
+  content: "# This is a first post",
+};
+
 const PostContent = () => {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
   return (
     <article>
-      <PostHeader />
-      Content
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      {DUMMY_POST.content}
     </article>
   );
 };
