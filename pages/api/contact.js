dda63009a525023781c2ea89819cf1bbca70c36e
nelo <nelokwaz@gmail.com>
2022-05-20 10:19:39 +0100

    created the contact api and added validation logic

---
+++ pages/api/contact.js
@@ -0,0 +1,17 @@
+function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid Input" });
+      return;
+    }
+  }
+}
